0f55cf2742631953e05bc637b74220e8efd9282b
Rátki Barnabás <barna.ratki@gmail.com>
2018-05-25 19:05:43 +0200

    ChargePush done

---
+++ Code/AndThereWereNone/Assets/ChargePush/Scripts/BlaBla.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlaBla : MonoBehaviour
+{
+
+    public string FullText = "Bla ...   Bla";
+    public string TextNow = "";
+    public float timeElapsed;
+    public float lettersPerSec;
+
+    // Use this for initialization
+    void Start ()
+	{
+	    timeElapsed = 0f;
+	    TextNow = "";
+	}
+
+    public void OnDisable()
+    {
+        timeElapsed = 0f;
+        TextNow = "";
+    }
+
+    // Update is called once per frame
+    void Update ()
+	{
+	    timeElapsed += Time.deltaTime;
+	    var charCount = (int)(timeElapsed * lettersPerSec);
+	    if (charCount > FullText.Length)
+	    {
+	        charCount = 0;
+	        timeElapsed = 0f;
+	    }
+
+	    TextNow = FullText.Substring(0, charCount);
+
+	    gameObject.GetComponent<Text>().text = TextNow;
+	}
+}
--- Code/AndThereWereNone/Assets/ChargePush/Scripts/ChargePushControll.cs
+++ Code/AndThereWereNone/Assets/ChargePush/Scripts/ChargePushControll.cs
@@ -1,16 +1,139 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ChargePushControll : MonoBehaviour {
+public class ChargePushControll : MonoBehaviour
+{
+
+    public GameObject Hand;
+    public GameObject Face;
+    public GameObject Dialog;
+
+    public Slider PushProgress;
+
+    //face
+    public KeyCode KeyBoardChargeControll;
+    public bool IsLooking;
+    public bool IsCharging;
+
+    public float PercentPerSecond;
+    public bool IsOver;
+
+    public float MaxNotLookingSec;
+    public float MaxLookingSec;
+    public float NextNotLookingSec;
+    public float NextLookingSec;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    IsLooking = false;
+	    IsCharging = false;
+	    IsOver = false;
+
+        RestartLookingSequence();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    public void RestartLookingSequence()
+    {
+        NextNotLookingSec = Random.Range(0.7f, MaxNotLookingSec);
+        NextLookingSec = Random.Range(0.7f, MaxLookingSec);
+
+        StartCoroutine(LookingSequence());
+    }
+
+    public IEnumerator LookingSequence()
+    {
+        IsLooking = false;
+        Dialog.SetActive(true);
+        Face.GetComponent<Animator>().SetBool("Looking",false);
+        yield return new WaitForSeconds(NextNotLookingSec);
+        Face.GetComponent<Animator>().SetBool("Looking", true);
+        yield return new WaitForSeconds(1f);
+        Dialog.SetActive(false);
+        IsLooking = true;
+        yield return new WaitForSeconds(NextLookingSec);
+
+        RestartLookingSequence();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+	{
+	    KeyBoardControll();
+	    TouchControll();
+
+	    if (IsCharging && IsLooking)
+	    {
+            //U lose
+            Debug.Log("Lost");
+	        IsOver = true;
+	    }
+
+	    if (!IsOver && IsCharging)
+	    {
+	        if (PushProgress.value == 1f)
+	        {
+                //WON
+                Debug.Log("Won");
+	        }
+	        else
+	        {
+	            PushProgress.value += PercentPerSecond * Time.fixedDeltaTime;
+	        }
+
+	    }
 	}
+
+    public void Charging(bool started)
+    {
+        Hand.GetComponent<Animator>().SetBool("Pushing", started);
+        IsCharging = started;
+    }
+
+    public void TouchControll()
+    {
+        foreach (var touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                Charging(true);
+            }
+            else if (touch.phase == TouchPhase.Canceled || touch.phase == TouchPhase.Ended)
+            {
+                Charging(false);
+            }
+        }
+    }
+
+    public void KeyBoardControll()
+    {
+        if (Input.GetKeyDown(KeyBoardChargeControll))
+        {
+            Charging(true);
+        }
+
+        else if (Input.GetKeyUp(KeyBoardChargeControll))
+        {
+            Charging(false);
+        }
+
+        else if (Input.GetKey(KeyBoardChargeControll) && !IsCharging)
+        {
+            Charging(true);
+        }
+
+        else if(!Input.GetKey(KeyBoardChargeControll) && IsCharging)
+        {
+            Charging(false);
+        }
+    }
+
+    public void SwitchLooking()
+    {
+        IsLooking = !IsLooking;
+        Face.GetComponent<Animator>().SetBool("Looking", IsLooking);
+    }
+
 }
