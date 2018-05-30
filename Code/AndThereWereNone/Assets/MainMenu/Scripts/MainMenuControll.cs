1356087bb11441d5ae97843963d9db8cdf2714f1
Rátki Barnabás <barna.ratki@gmail.com>
2018-05-30 23:55:21 +0200

    MainMenu and soundtrack

---
+++ Code/AndThereWereNone/Assets/MainMenu/Scripts/MainMenuControll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.SharedAssets;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class MainMenuControll : MonoBehaviour
+{
+
+    public Slider VolSlider;
+
+    public void StartPressed()
+    {
+        var res = JsonConvert.SerializeObject(new TextSource()
+        {
+            Panels = new List<Panel>() {new Panel() {Strings = new Dictionary<string, string>() {{"asd", "asd"},{"ass","my"}}},new Panel() {Strings = new Dictionary<string, string>(){{"my","ass"}}}}
+        });
+
+        File.WriteAllText("Resources/enb.json",res);
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+
+    public void VolChanged()
+    {
+        GameObject.FindGameObjectWithTag("SoundTrack").gameObject.GetComponent<AudioSource>().volume = VolSlider.value;
+    }
+}
