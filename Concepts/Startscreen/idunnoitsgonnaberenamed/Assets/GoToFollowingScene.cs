d24435477a06bff3b5b7a849603eab8004ea75c7
Xeena2812 <levi2812@outlook.hu>
2018-05-31 18:55:18 +0200

    The best commit mesage ive ever written

---
+++ Concepts/Startscreen/idunnoitsgonnaberenamed/Assets/GoToFollowingScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToFollowingScene : MonoBehaviour {
+
+    public void OnPress()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
---
+++ Concepts/Startscreen/idunnoitsgonnaberenamed/Assets/Quitbuttpress.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitbuttpress : MonoBehaviour {
+
+    public void WhenpressedQuit()
+    {
+        Application.Quit();
+    }
+}
